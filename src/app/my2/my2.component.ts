34e30541355f730eb1cef1373b65119f07f8f531
Error <fearerror@yandex.ru>
2020-01-23 13:28:22 +0300

    Creating a component with Angular CLI.

---
+++ src/app/my2/my2.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my2',
+  templateUrl: './my2.component.html',
+  styleUrls: ['./my2.component.css']
+})
+export class My2Component {
+}
