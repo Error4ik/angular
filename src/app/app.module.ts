73fead1cbb55fba24bff31f01b8eae70fc0400cf
Error <fearerror@yandex.ru>
2020-01-26 22:05:59 +0300

    Two way binding.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {MyComponent} from './my/my.component';
 import { My2Component } from './my2/my2.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { My2Component } from './my2/my2.component';
     My2Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/my2/my2.component.ts
+++ src/app/my2/my2.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-my2',
@@ -6,4 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./my2.component.css']
 })
 export class My2Component {
+  title = 'Hello';
 }
