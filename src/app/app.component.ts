e2fbd020b5efb318b0d1acd573ff4aed4cf0ba40
Error <fearerror@yandex.ru>
2020-01-27 18:14:58 +0300

    Create components.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,25 +6,5 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular';
-  inputValue = '';
-
-  constructor() {
-  }
-
-  onInput = (event) => {
-    this.inputValue = event.target.value;
-  };
-
-  onCLick = () => {
-    console.log('Click');
-  };
-
-  onInput2 = (event: KeyboardEvent) => {
-    this.inputValue = (event.target as HTMLInputElement).value;
-  };
-
-  onBlur = (str: string) => {
-    this.inputValue = str;
-  };
+  title = 'Angular Components';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {MyComponent} from './my/my.component';
-import { My2Component } from './my2/my2.component';
 import {FormsModule} from '@angular/forms';
+import { PostFormComponent } from './post-form/post-form.component';
+import { PostComponent } from './post/post.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyComponent,
-    My2Component
+    PostFormComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
