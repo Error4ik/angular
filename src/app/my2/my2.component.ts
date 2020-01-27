b3aa3b18175dbc42d3570d51d855df5cac817580
Error <fearerror@yandex.ru>
2020-01-27 17:44:06 +0300

    Pipes.

--- src/app/my2/my2.component.ts
+++ src/app/my2/my2.component.ts
@@ -6,15 +6,17 @@ import {Component} from '@angular/core';
   styleUrls: ['./my2.component.css']
 })
 export class My2Component {
-  backgroundToggle = false;
+  // backgroundToggle = false;
+  //
+  // toggle: any = false;
+  //
+  // array = [1, 1, 2, 3, 5, 8, 13, 21];
+  //
+  // object = [
+  //   {title: 'heads'},
+  //   {title: 'legs'},
+  //   {title: 'arms'}
+  // ];
 
-  toggle: any = false;
-
-  array = [1, 1, 2, 3, 5, 8, 13, 21];
-
-  object = [
-    {title: 'heads'},
-    {title: 'legs'},
-    {title: 'arms'}
-  ];
+  now: Date = new Date();
 }
