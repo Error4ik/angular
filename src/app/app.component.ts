092f6c3ec7143f7da59b93526d125cb9c7a4935b
Error <fearerror@yandex.ru>
2020-01-29 21:12:13 +0300

    Parameter passingю

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import {Component} from '@angular/core';
 
+export interface Post {
+  title: string;
+  text: string;
+  id?: number;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular Components';
+  name = 'Test';
+
+  posts: Post[] = [
+    {
+      title: 'Ангулар.',
+      text: 'Выучить ангулар компонент.',
+      id: 1
+    },
+    {
+      title: 'TypeScript',
+      text: 'Выучить TypeScript',
+      id: 2
+    }
+  ];
 }
