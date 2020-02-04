1f1c18dfb205a4e1108375f3017af9c16780e02c
Error <fearerror@yandex.ru>
2020-02-04 23:28:23 +0300

    'Directives'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,55 +1,13 @@
 import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 
-export interface Post {
-  title: string;
-  text: string;
-  id?: number;
-}
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  name = 'Test';
-
-  posts: Post[] = [
-    {
-      title: 'Ангулар.',
-      text: 'Выучить ангулар компонент.',
-      id: 1
-    },
-    {
-      title: 'TypeScript',
-      text: 'Выучить TypeScript',
-      id: 2
-    }
-  ];
-
-  updatePosts(post: Post) {
-    this.posts.unshift(post);
-  }
-
-  removePost(id: number) {
-    this.posts = this.posts.filter(p => p.id !== id);
-  }
+  name = 'Angular Directives';
 
   ngOnInit(): void {
-    // // TODO ChangeDetectionStrategy.Default.
-    // setTimeout(() => {
-    //   console.log('Timeout');
-    //   this.posts[0].title = 'Changed!';
-    // }, 5000);
-
-    // // TODO ChangeDetectionStrategy.onPush.
-    // setTimeout(() => {
-    //   console.log('Timeout');
-    //   this.posts[0] = {
-    //     title: 'Changed',
-    //     text: 'Changed',
-    //     id: 5
-    //   };
-    // }, 5000);
   }
 }
