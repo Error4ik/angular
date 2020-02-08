eda813cd3877eed58c743d6cdb338442027e34d4
Error <fearerror@yandex.ru>
2020-02-08 21:53:32 +0300

    Services.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,5 @@
 import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
-import {Observable} from 'rxjs';
-export interface Post {
-  title: string;
-  text: string;
-}
+import {AppCounterService} from './services/app-counter.service';
 
 @Component({
   selector: 'app-root',
@@ -11,25 +7,11 @@ export interface Post {
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  name = 'Angular Pipes';
+  name = 'Angular Services';
 
-  promise: Promise<string> = new Promise<string>(resolve => {
-    setTimeout(() => {
-      resolve('resolved!');
-    }, 3000);
-  });
-
-  date$: Observable<Date> = new Observable<Date>(obs => {
-    setInterval(() => {
-      obs.next(new Date());
-    }, 1000);
-  });
-
-  date = new Date();
+  constructor(private appCounterService: AppCounterService) {
+  }
 
   ngOnInit(): void {
-    this.date$.subscribe(date => {
-      this.date = date;
-    });
   }
 }
