1eda02244cb1c950cc7f7378e62d68af6696200c
Error <fearerror@yandex.ru>
2020-02-08 22:01:00 +0300

    Local service.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,17 @@
 import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 import {AppCounterService} from './services/app-counter.service';
+import {LocalCounterService} from './services/local-counter.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [LocalCounterService]
 })
 export class AppComponent implements OnInit {
   name = 'Angular Services';
 
-  constructor(private appCounterService: AppCounterService) {
+  constructor(private appCounterService: AppCounterService, private localCounterService: LocalCounterService) {
   }
 
   ngOnInit(): void {
