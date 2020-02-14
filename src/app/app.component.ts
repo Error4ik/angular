ed446bd84ca12aed94d9cbc59324c8fd90b738a1
Error <fearerror@yandex.ru>
2020-02-14 23:20:06 +0300

    Create group.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,11 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     this.form = new FormGroup({
       email: new FormControl('', [Validators.email, Validators.required]),
-      password: new FormControl('', [Validators.required, Validators.minLength(6)])
+      password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+      address: new FormGroup({
+        country: new FormControl('ru'),
+        city: new FormControl('', Validators.required)
+      })
     });
   }
 
