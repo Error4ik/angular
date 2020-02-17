414e9a6d2952100067c9e2b6b40c047870ef63ac
Error <fearerror@yandex.ru>
2020-02-17 23:44:58 +0300

    NgModel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 import {FormArray, FormControl, FormGroup, Validators} from '@angular/forms';
-import {AppValidators} from './app.validators';
 
 @Component({
   selector: 'app-root',
@@ -8,48 +7,13 @@ import {AppValidators} from './app.validators';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  name = 'Angular Forms';
-  form: FormGroup;
+  name = 'Angular NgModels';
+  appState = 'on';
 
   ngOnInit(): void {
-    this.form = new FormGroup({
-      email: new FormControl('',
-        [Validators.email,
-          Validators.required,
-          AppValidators.restrictedEmails],
-        [AppValidators.uniqueEmail]),
-      password: new FormControl('', [Validators.required, Validators.minLength(6)]),
-      address: new FormGroup({
-        country: new FormControl('ru'),
-        city: new FormControl('', Validators.required)
-      }),
-      skills: new FormArray([])
-    });
   }
 
-  send() {
-    if (this.form.valid) {
-      console.log('Form:', this.form);
-      const formData = {...this.form.value};
-      console.log('FormData:', formData);
-    }
-
-    this.form.reset();
-  }
-
-  setCapital() {
-    const cityMap = {
-      ru: 'Moscow',
-      ua: 'Kiev',
-      us: 'Washington'
-    };
-
-    const city = cityMap[this.form.get('address').get('country').value];
-    this.form.patchValue({address: {city}});
-  }
-
-  addSkill() {
-    const skillControl = new FormControl('', Validators.required);
-    (this.form.get('skills') as FormArray).push(skillControl);
+  handleChange() {
+    console.log(this.appState);
   }
 }
