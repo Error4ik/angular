51fea3210118a4858acab57e38841f019ccd6065
Error <fearerror@yandex.ru>
2020-02-19 23:31:41 +0300

    Observe.

--- src/app/todo.service.ts
+++ src/app/todo.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
-import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
+import {HttpClient, HttpEventType, HttpHeaders, HttpParams} from '@angular/common/http';
 import {Observable, throwError} from 'rxjs';
-import {catchError, delay} from 'rxjs/operators';
+import {catchError, delay, map, tap} from 'rxjs/operators';
 
 export interface Todo {
   id?: number;
@@ -28,18 +28,34 @@ export class TodoService {
     params = params.append('_limit', '5');
     params = params.append('custom', 'anything');
     return this.httpClient.get<Todo[]>('https://jsonplaceholder.typicode.com/todos?', {
-      params
+      params,
+      observe: 'response'
     })
+      .pipe(map(response => {
+        console.log(response);
+        return response.body;
+      }))
       .pipe(delay(500));
   }
 
-  removeTodo(id: number): Observable<void> {
-    return this.httpClient.delete<void>(`https://jsonplaceholder.typicode.com/todos/${id}`);
+  removeTodo(id: number): Observable<any> {
+    return this.httpClient.delete<void>(`https://jsonplaceholder.typicode.com/todos/${id}`, {
+      observe: 'events'
+    }).pipe(tap(event => {
+      if (event.type === HttpEventType.Sent) {
+        console.log('Sent', event);
+      }
+      if (event.type === HttpEventType.Response) {
+        console.log('Response', event);
+      }
+    }));
   }
 
   completeTodo(id: number): Observable<Todo> {
     return this.httpClient.put<Todo>(`https://jsonplaceholder.typicode.com/todos/${id}`, {
       completed: true
+    }, {
+      responseType: 'json' // default
     }).pipe(catchError(error => {
       console.log('Error:', error.message);
       error.message = 'Something went wrong...';
