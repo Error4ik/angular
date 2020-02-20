c3e95c3ca9917033922c4f711471c784934650ff
Error <fearerror@yandex.ru>
2020-02-20 23:34:21 +0300

    Parameters two.

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {PostsService} from '../posts.service';
-import {ActivatedRoute, Params} from '@angular/router';
+import {ActivatedRoute, Params, Router} from '@angular/router';
 
 @Component({
   selector: 'app-posts',
@@ -11,12 +11,24 @@ export class PostsComponent implements OnInit {
 
   showId = false;
 
-  constructor(private postService: PostsService, private route: ActivatedRoute) { }
+  constructor(private postService: PostsService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
     this.route.queryParams.subscribe((params: Params) => {
       this.showId = !!params.showId;
     });
+
+    this.route.fragment.subscribe((fragment) => {
+      console.log('Fragment', fragment);
+    });
   }
 
+  showIdProgram() {
+    this.router.navigate(['/posts'], {
+      queryParams: {
+        showId: true
+      },
+      fragment: 'program-fragment!'
+    });
+  }
 }
