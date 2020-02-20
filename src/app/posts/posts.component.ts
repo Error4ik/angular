190dae221cc3205e6d7feebeebaf4a2828277e9d
Error <fearerror@yandex.ru>
2020-02-20 23:28:09 +0300

    Parameters one.

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {PostsService} from '../posts.service';
+import {ActivatedRoute, Params} from '@angular/router';
 
 @Component({
   selector: 'app-posts',
@@ -8,9 +9,14 @@ import {PostsService} from '../posts.service';
 })
 export class PostsComponent implements OnInit {
 
-  constructor(private postService: PostsService) { }
+  showId = false;
+
+  constructor(private postService: PostsService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.queryParams.subscribe((params: Params) => {
+      this.showId = !!params.showId;
+    });
   }
 
 }
