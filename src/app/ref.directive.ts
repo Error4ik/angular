0209d6c59301c3666cadf65c3966dab62b14427a
Error <fearerror@yandex.ru>
2020-02-22 12:13:27 +0300

    Dynamic components 2.

---
+++ src/app/ref.directive.ts
@@ -0,0 +1,6 @@
+import {Directive, ViewContainerRef} from '@angular/core';
+
+@Directive({selector: '[appRef]'})
+export class RefDirective {
+  constructor(public containerRef: ViewContainerRef) {}
+}
